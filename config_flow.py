4fe0e8138edcaa341060d7ffe925152cc4f85002
Martin Grund <martin.grund@databricks.com>
2021-10-19 16:08:46 +0200

    Initial HS implementation
    Format
    
    PutValue for Gira HS
    
    Fixing connection string
    
    Auth Flow

---
+++ config_flow.py
@@ -0,0 +1,95 @@
+import typing
+from const import DEFAULT_HOST
+from gira import AuthException, HomeServer
+from homeassistant.core import callback
+
+from homeassistant.data_entry_flow import FlowResult
+from . import DOMAIN
+from homeassistant import config_entries
+import voluptuous as vol
+import logging
+
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, CONF_HOST
+
+_LOGGER = logging.getLogger(__name__)
+
+OptStrAnyMap = typing.TypeVar("OptStrAnyMap", dict[str, typing.Any], None)
+OptStrStrMap = typing.TypeVar("OptStrStrMap", dict[str, str], None)
+
+
+class GiraHomeAssistantIntegration(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a FRITZ!Box Tools config flow."""
+
+    VERSION = 1
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._name = "GiraHomeServer"
+        self._host: str
+        self._username: str
+        self._password: str
+        self._gira: HomeServer
+
+    async def init_home_server(self) -> typing.Optional[str]:
+        """Initialize the connection and check if we can auth"""
+        if not self._host or not self._username or not self._password:
+            return "missing_config_value"
+
+        self._gira = HomeServer(self._username, self._password, self._host)
+        try:
+            self._gira.login()
+            return None
+        except AuthException:
+            return "invalid_auth"
+        except:
+            return "unknown_error"
+
+    async def async_step_user(self, user_input: OptStrAnyMap = None):
+        """Configuration of the user."""
+        if user_input is None:
+            return self._show_setup_form_init()
+        self._host = user_input[CONF_HOST]
+        self._username = user_input[CONF_USERNAME]
+        self._password = user_input[CONF_PASSWORD]
+
+        if not (error := await self.init_home_server()):
+            if await self.async_check_configured_entry():
+                error = "already_configured"
+
+        if error:
+            return self._show_setup_form_init({"base": error})
+
+        pass
+
+    async def async_check_configured_entry(self) -> config_entries.ConfigEntries | None:
+        """Check if entry is configured."""
+        for entry in self._async_current_entries(include_ignore=False):
+            if entry.data[CONF_HOST] == self._host:
+                return entry
+        return None
+
+    @callback
+    def _async_create_entry(self) -> FlowResult:
+        """Async create flow handler entry."""
+        return self.async_create_entry(
+            title=self._name,
+            data={
+                CONF_HOST: self._host,
+                CONF_PASSWORD: self._username,
+                CONF_USERNAME: self._password,
+            },
+        )
+
+    def _show_setup_form_init(self, errors: OptStrStrMap = None) -> FlowResult:
+        """Show the setup form to the user."""
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional(CONF_HOST, default=DEFAULT_HOST): str,
+                    vol.Required(CONF_USERNAME): str,
+                    vol.Required(CONF_PASSWORD): str,
+                }
+            ),
+            errors=errors or {},
+        )
---
+++ gira.py
@@ -0,0 +1,178 @@
+from collections import UserDict
+import json
+import requests
+import logging
+import typing
+from requests.auth import HTTPBasicAuth
+
+
+class AuthException(Exception):
+    pass
+
+
+class FetchException(Exception):
+    pass
+
+
+class Accessory(object):
+    def __init__(
+        self,
+        uid: str,
+        display_name: str,
+        location: str,
+        channel_type: str,
+        data_points: typing.List,
+    ) -> None:
+        super().__init__()
+        self._uid = uid
+        self._display_name = display_name
+        self._location = location
+        self._channel_type = channel_type
+        self._data_points = data_points
+
+    @property
+    def uid(self) -> str:
+        return self._uid
+
+    @property
+    def display_name(self) -> str:
+        return self._display_name
+
+    @property
+    def location(self) -> str:
+        return self._location
+
+    @property
+    def channel_type(self) -> str:
+        return self._channel_type
+
+    @property
+    def data_points(self) -> typing.List:
+        return self._data_points
+
+    def __repr__(self) -> str:
+        return f"<Accessory: {self.display_name}({self.uid})@{self.location}>"
+
+
+AccessoryList = list[Accessory]
+V = typing.TypeVar("V", str, int, float, bool, None)
+
+
+class HomeServer(object):
+
+    CLIENT = "HomeAssistant"
+
+    def __init__(self, username, password, host) -> None:
+        super().__init__()
+        # Create empty token
+        self.token = ""
+        self.username = username
+        self.password = password
+        self.host = host
+
+    def login(self) -> bool:
+        """Login to the Gira HS and obtain a new toke for the application"""
+        endpoint = f"https://{self.host}/api/clients"
+        logging.debug(f"Connecting to: {endpoint}")
+        logging.debug(f'{{client:"{HomeServer.CLIENT}"}}')
+        response = requests.post(
+            endpoint,
+            data=json.dumps({"client": HomeServer.CLIENT}),
+            auth=HTTPBasicAuth(self.username, self.password),
+            verify=False,
+        )
+        if response.status_code != 200:
+            logging.error(f"Error loging in {response.text}")
+            raise AuthException(f"Could not login to Gira HS at {self.host}")
+        data = response.json()
+        self.token = data["token"]
+        logging.debug("Received token from HS.")
+        return True
+
+    def fetchAllObjects(self) -> None:
+        """Fetches all configuration objects from Gira HS. After all data is fetched,
+        the data can be transformed into local objects."""
+
+        endpoint = f"https://{self.host}/api/uiconfig"
+        response = requests.get(
+            endpoint,
+            params={"token": self.token, "expand": "parameters,locations,trades"},
+            verify=False,
+        )
+        if response.status_code != 200:
+            logging.error(f"Error fetching KOs {response.text}")
+            raise FetchException("Could not fetch KOs")
+        self.data = response.json()
+
+    def transformAccessories(self) -> AccessoryList:
+        """Transforms the local list of acessories into items recognizable by the system"""
+        logging.debug("Transforming all accessories")
+        location_map = self.buildLocationMap(self.data["locations"])
+        result = []
+        for fn in self.data["functions"]:
+            loc = location_map.get(fn["uid"], "None")
+            result.append(
+                Accessory(
+                    fn["uid"],
+                    fn["displayName"],
+                    loc,
+                    fn["channelType"],
+                    fn["dataPoints"],
+                )
+            )
+        return result
+
+    def buildLocationMap(self, data: typing.List, parent="") -> typing.Dict:
+        """Recursively processes the input map to build a proper UID to location naming"""
+        prefix = parent + "/" if len(parent) > 0 else parent
+        result = {}
+        for d in data:
+            new_parent = f"{prefix}{d['displayName']}"
+            for f in d["functions"]:
+                result[f] = new_parent
+            tmp = self.buildLocationMap(d["locations"], new_parent)
+            result.update(tmp)
+        return result
+
+    def getValue(self, uid: str) -> V:
+        endpoint = f"https://{self.host}/api/values/{uid}"
+        response = requests.get(endpoint, params={"token": self.token}, verify=False)
+        if response.status_code != 200:
+            raise FetchException(f"Could not load value for {uid}")
+        for v in response.json()["values"]:
+            if v["uid"] == uid:
+                return v["value"]
+        return None
+
+    def setValue(self, uid: str, value: V) -> None:
+        endpoint = f"https://{self.host}/api/values/{uid}"
+        response = requests.put(
+            endpoint, data={"value": value}, params={"token": self.token}, verify=False
+        )
+        if response.status_code != 200:
+            raise FetchException(f"Could not load value for {uid}")
+        for v in response.json()["values"]:
+            if v["uid"] == uid:
+                return v["value"]
+        return None
+
+
+if __name__ == "__main__":
+    import os
+
+    logging.basicConfig(level=logging.DEBUG)
+    logging.debug("Start...")
+    logging.debug(f"Connecting with User : {os.environ.get('HS_USERNAME')}")
+    hs = HomeServer(
+        os.environ.get("HS_USERNAME"), os.environ.get("HS_PASSWORD"), "192.168.178.5"
+    )
+    hs.login()
+    hs.fetchAllObjects()
+
+    objs = hs.transformAccessories()
+    for o in objs:
+        if o.uid == "f287":
+            fn = o.data_points[0]["uid"]
+            print(hs.getValue(fn))
+
+    # logging.debug(hs.transformAccessories())
